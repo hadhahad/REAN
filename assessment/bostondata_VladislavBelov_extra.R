c4c93af7b4cda531eeceb55310966f93dfbd61d4
vladislav.belov <belovvla@fjfi.cvut.cz>
2018-01-12 02:45:32 +0100

    extra finished

--- assessment/bostondata_VladislavBelov_extra.R
+++ assessment/bostondata_VladislavBelov_extra.R
@@ -44,7 +44,7 @@ summary(logboston_lm)
 opar <- par(mfrow=c(2,2))
 plot(logboston_lm)
 par(opar)
-# Rezidua jsou relativně symetrické vzhledem k 0, Q-Q plot také vypadí docela rozumně.
+# Rezidua jsou relativně symetrická vzhledem k 0, Q-Q plot také vypadí docela rozumně.
 
 ### (2) Graf s regresní křivkou pro nový model a konfidenčními intervaly na hladině významnosti 5%: ###
 new_nox <- data.frame(nox = seq(0.3850,0.8710,0.0009925))
@@ -99,7 +99,7 @@ logtrans_p1
 exp_beta1 <- exp(coef(logboston_lm)[2]*0.1)  
 # Je to kladné číslo menší než jedna vyjádřující poměr E[Y|X=x+0.1]/E[Y|X=x].
 # Tedy (1-exp_beta1)*100 % vyjadřuje o kolik procent klesne cena nemovitosti 
-# při nárustu 'nox' o 0.1.
+# při nárůstu 'nox' o 0.1.
 (1-exp_beta1)*100
 # Což je přibližně 16.97 %.
 
@@ -108,6 +108,7 @@ exp_beta1 <- exp(coef(logboston_lm)[2]*0.1)
 ############ 
 
 dev.off()
+new_nox <- data.frame(nox = seq(0.3850,0.8710,0.0009925))
 
 ### (1) Lineární transformace. ###
 lin_boston_lm <- lm(medv ~ poly(nox, degree=1, raw=TRUE), data = newBoston)
@@ -170,16 +171,14 @@ par(opar)
 opar <- par(mfrow=c(2,2))
 logsplines_boston_lm <- lm(log(medv) ~ bs(nox,knots=c(0.695),degree=1,Boundary.knots=c(0.385, 0.8710)), data = newBoston)
 summary(logsplines_boston_lm)
-# Logaritmizací odezvy jsme se docíleli zvětšení koeficientu determinace a zlepšenít tvaru Q-Q plotu.
+# Logaritmizací odezvy jsme se docíleli zvětšení koeficientu determinace a zlepšení tvaru Q-Q plotu.
 plot(logsplines_boston_lm)
 par(opar)
-# Nyní Q-Q Plot vypadá o něco lepší, stále ale jsou přítomny outliery a leverage pointy.
-# Navíc se zvětšila signifikance poslední proměnné.
 
 ### Nakreslíme si Scatter Plot s konfidenčními a prediction intervaly na hladine významnosti 5 %: ###
 new_conf = predict(logsplines_boston_lm, newdata = new_nox, interval = "confidence", level=0.95)
 new_pred = predict(logsplines_boston_lm, newdata = new_nox, interval = "prediction", level=0.95)
-boston_crime_final_p1<- ggplot(newBoston, aes(x=nox, y=log(medv))) +
+boston_simple_final_p1<- ggplot(newBoston, aes(x=nox, y=log(medv))) +
   geom_point(size=1, alpha=0.7) +
   labs(color="Lines:") + 
   geom_line(aes(x=seq(0.3850,0.8710,0.0009925), y=new_conf[,1], colour = "Fit")) + 
@@ -188,11 +187,11 @@ boston_crime_final_p1<- ggplot(newBoston, aes(x=nox, y=log(medv))) +
   geom_line(aes(x=seq(0.3850,0.8710,0.0009925), y=new_pred[,2], colour = "Prediction Interval"), linetype=2) + 
   geom_line(aes(x=seq(0.3850,0.8710,0.0009925), y=new_pred[,3], colour = "Prediction Interval"), linetype=2) + 
   theme_bw() +
-  xlab("Per Capita Crime Rate by Town") +
+  xlab("Nitrogen Oxides Concentration (parts per 10 million)") +
   ylab("Log - Mean Value of Owner-Occupied Homes") +
-  ggtitle("Mean Value of Owner-Occupied Homes vs. Criminality") +
+  ggtitle("Mean Value of Owner-Occupied Homes vs. Nitrogen Oxides Concentration") +
   coord_cartesian(xlim=c(0.3850, 0.8710), ylim=c(1.5, 4))
-boston_crime_final_p1
+boston_simple_final_p1
 
 ### Plot efektů: ###
 plot(allEffects(logsplines_boston_lm))
@@ -203,6 +202,140 @@ shapiro.test(residuals(logsplines_boston_lm))
 
 ### Lilliefors (Kolmogorov-Smirnov) test for normality: ###
 library(nortest)
-lillie.test(residuals(logsplines_boston_lm)) # Analogicky, ale na hladině 1 % by to šlo příjmout.
+lillie.test(residuals(logsplines_boston_lm)) # Analogicky.
+
+
+
+############
+### Q12: ###
+############
+
+dev.off()
+boston_all_lm <- lm(log(medv) ~ ., data = newBoston)
+opar <- par(mfrow=c(2,2))
+plot(boston_all_lm)
+par(opar)
+summary(boston_all_lm)  # Model se všemi proměnnými najednou je přetížený.
+
+### Pro výběr ze všech možných vysvětlujících proměnných použijeme balík 'leaps' a kritéria Cp/r2. ###
+summary(newBoston)
+leapsstatCp <- leaps(x=newBoston[,c(1:13)], y=newBoston[,14], names=names(newBoston)[c(1:13)], method="Cp")
+leapsstatr2 <- leaps(x=newBoston[,c(1:13)], y=newBoston[,14], 
+                     names=names(newBoston)[c(1:13)], method="r2")
+CpParams <- leapsstatCp$size; CpValues <- leapsstatCp$Cp
+r2Params <- leapsstatr2$size; r2Values <- leapsstatr2$r2
+opar <- par(mfrow=c(1,2))
+plot(CpValues ~ CpParams)
+plot(r2Values ~ r2Params) 
+par(opar)
+
+# Vybereme proměnné s indexy 1, 5, 6, 8, 9, 10, 11, 13 (podle grafů je vidět, že existují i lepší
+# modely, ale chceme dobrý poměr cena/výkon.
+names(Boston)[c(1, 5, 6, 8, 9, 10, 11, 13)]
+leaps <- regsubsets(log(medv) ~ crim + nox + rm + dis + rad + tax + ptratio + lstat, data = newBoston, nbest=10)
+summary(leaps)
+plot(leaps,scale="Cp")
+plot(leaps,scale="adjr2")
+subsets(leaps, statistic="bic") 
+subsets(leaps, statistic="adjr2") 
+dev.off()
+
+boston_lm_final <- lm(log(medv) ~ crim + nox + rm + dis + rad + tax + ptratio + lstat, data = newBoston)
+boston_lm_AIC <- stepAIC(boston_lm_final)
+summary(boston_lm_AIC) # AIC už nic nevyhodí - všechny zvolené proměnné jsou signifikantní.
+# Proto zvolíme mode boston_lm_final.
+
+# Na grafu Residuals vs. Fitted lze pozorovat symetrii.
+op <- par(mfrow=c(2,2))
+plot(boston_lm_final)
+par(opar)
+
+# Z Shapiro-Wilkova testu neplyne normalita reziduí.
+shapiro.test(residuals(boston_lm_final))
+
+# Breusch-Paganův test ukazuje na heteroskedascicitu.
+bptest(boston_lm_final)
+
+
+############
+### Q13: ###
+############
+
+# Z diagramu níže a z tvaru kovarianční matice vyplývá, že mezi 
+# vybranými proměnnými není významná kolinearita. Vzhledem ke korelacím
+# ostatních proměnných si dovolíme říct, že proměnné byly vybrány relativně dobře.
+# Lze ale mezi zvolenými proměnnými pozorovat jakousi nelineární závislost. 
+pairs(medv ~ rm + ptratio + lstat, data = newBoston) 
+cor(cbind(newBoston$crim,newBoston$zn,newBoston$indus,newBoston$chas,newBoston$nox,
+          newBoston$rm,newBoston$age,newBoston$dis,newBoston$rad,newBoston$tax,
+          newBoston$black,newBoston$ptratio,newBoston$lstat))
+# Q05 obsahuje zobrazení matice kolinearity.
+
+# Inflace variance pro vybraný model je příliš velká pro proměnné 'rad' a 'tax'. 
+vif(boston_lm_final)
+# Zkusíme ubrat proměnnou 'rad':
+boston_lm_final <- update(boston_lm_final, . ~ . - rad)
+vif(boston_lm_final) 
+# vif už dává dobrý výsledek, ale na druhou stranu teď proměnná 'tax' už není signifikantní. 
+summary(boston_lm_final)
+boston_lm_final <- update(boston_lm_final, . ~ . - tax)
+# Tedy ji také z finálního modelu ubereme.
+summary(boston_lm_final)
+# Nyní jsou všechny proměnné velice signifikantní, koeficient determinace je stále vysoký.
+
+# Index podmíněnosti je o hodně menší 30
+# (wiki: "If the condition number is above 30, the regression 
+# may have significant multicollinearity").
+kappa(cor(cbind(newBoston$crim, newBoston$nox, newBoston$rm, newBoston$dis,
+                newBoston$ptratio, newBoston$lstat)),exact=T)
+
+
+############
+### Q14: ###
+############
+
+# Zvolený model zahrnuje v sobě proměnnou 'nox'.
+summary(boston_lm_final)
+opar <- par(mfrow=c(2,2))
+plot(boston_lm_final)
+par(opar)
+
+exp_beta1 <- exp(coef(boston_lm_final)[3]*0.1) 
+# Je to kladné číslo menší než jedna vyjádřující poměr E[Y|X=x+0.1, ...]/E[Y|X=x, ...].
+(1-exp_beta1)*100
+# Daný model predikuje pokles cen přibližně o 7.35 % při nárůstu koncentrace o 0.1.
+
+
+############
+### Q15: ###
+############
+
+### Výsledný model:
+summary(boston_lm_final)
+op <- par(mfrow=c(2,2))
+plot(boston_lm_final)
+par(opar)
+sigmaHat(boston_lm_final)
+# Tento model má přípustný koeficient determinace (>0.6).
+# Odchylka reziduí nění moc velká (~0.18).
+
+# Z Shapiro-Wilkova testu neplyne normalita reziduí.
+shapiro.test(residuals(boston_lm_final))
+
+# Breusch-Paganův test ukazuje na heteroskedascicitu.
+bptest(boston_lm_final)
+
+# Podívejme se na partial-regression grafy:
+avPlots(boston_lm_final)
+
+
+############
+### Q17: ###
+############
+
+# Myslím, že snížení koncentrace 'nox' by mělo větší vliv pozitivní vliv na ceny nemovitostí než potlačení kriminality.
+# Z Q14 a Q08 plyne, že zlepšení kvality vzduchu v okolí města by mohlo způsobit podstatné snížení cen.
+# Lze spekulovat, že kdybychom odstranili pozorovaní 'nox' >0.75, dostali bychom ještě větší procento.
+
 
 
